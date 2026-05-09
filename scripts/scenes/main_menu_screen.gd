27f83efabee657abd7272b9053739613cb8fa313
vbird <745433716@qq.com>
2026-05-09 14:13:39 +0800

    feat: 添加继续游戏入口

--- scripts/scenes/main_menu_screen.gd
+++ scripts/scenes/main_menu_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var message_label: Label
+
 func _ready() -> void:
 	var title = Label.new()
 	title.text = "梁羽生群侠传"
@@ -12,6 +14,23 @@ func _ready() -> void:
 	start_button.pressed.connect(_start_new_game)
 	add_child(start_button)
 
+	var continue_button = Button.new()
+	continue_button.text = "继续游戏"
+	continue_button.position = Vector2(64, 152)
+	continue_button.pressed.connect(_continue_game)
+	add_child(continue_button)
+
+	message_label = Label.new()
+	message_label.position = Vector2(64, 208)
+	message_label.size = Vector2(600, 32)
+	add_child(message_label)
+
 func _start_new_game() -> void:
 	GameState.start_new_game()
 	SceneLoader.change_scene("res://scenes/mountain_pass.tscn")
+
+func _continue_game() -> void:
+	if not GameState.load_from_path("user://save_01.json"):
+		message_label.text = "没有可用存档。"
+		return
+	SceneLoader.change_scene(GameState.get_current_map_scene_path())
