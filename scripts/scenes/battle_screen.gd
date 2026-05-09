8ac821a6cdeea5de81eeeea7ded6d424e682a3da
vbird <745433716@qq.com>
2026-05-09 17:09:00 +0800

    feat: 接入回合战斗界面

--- scripts/scenes/battle_screen.gd
+++ scripts/scenes/battle_screen.gd
@@ -1,58 +1,111 @@
 extends Control
 
-const ActorStateScript = preload("res://scripts/domain/actor_state.gd")
-const MartialArtRecordScript = preload("res://scripts/domain/martial_art_record.gd")
 const CombatSystemScript = preload("res://scripts/systems/combat_system.gd")
 
+var title_label: Label
+var hero_hp_label: Label
+var enemy_hp_label: Label
 var output: Label
+var attack_button: Button
+var item_button: Button
+var retreat_button: Button
 var context: Dictionary = {}
+var battle_state = null
+var combat_system = CombatSystemScript.new()
 
 func _ready() -> void:
 	context = GameState.peek_battle_context()
+	combat_system.set_repository(DataRepository)
+	battle_state = combat_system.create_battle(GameState, context, DataRepository)
+	_create_ui()
+	_refresh()
+
+func _create_ui() -> void:
+	title_label = Label.new()
+	title_label.position = Vector2(32, 24)
+	title_label.size = Vector2(720, 32)
+	add_child(title_label)
+
+	hero_hp_label = Label.new()
+	hero_hp_label.position = Vector2(32, 72)
+	hero_hp_label.size = Vector2(320, 32)
+	add_child(hero_hp_label)
+
+	enemy_hp_label = Label.new()
+	enemy_hp_label.position = Vector2(380, 72)
+	enemy_hp_label.size = Vector2(320, 32)
+	add_child(enemy_hp_label)
 
 	output = Label.new()
-	output.text = "战斗开始：%s" % DataRepository.get_actor(_enemy_id()).get("name", "山道强人")
-	output.position = Vector2(32, 32)
-	output.size = Vector2(900, 240)
+	output.position = Vector2(32, 120)
+	output.size = Vector2(900, 220)
+	output.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	add_child(output)
 
-	var resolve_button = Button.new()
-	resolve_button.text = "基础剑法"
-	resolve_button.position = Vector2(32, 300)
-	resolve_button.pressed.connect(_resolve_battle)
-	add_child(resolve_button)
+	attack_button = Button.new()
+	attack_button.text = "基础剑法"
+	attack_button.position = Vector2(32, 372)
+	attack_button.size = Vector2(120, 40)
+	attack_button.pressed.connect(_on_attack_pressed)
+	add_child(attack_button)
 
-	var back_button = Button.new()
-	back_button.text = "暂退"
-	back_button.position = Vector2(180, 300)
-	back_button.pressed.connect(_retreat)
-	add_child(back_button)
+	item_button = Button.new()
+	item_button.text = "小还丹"
+	item_button.position = Vector2(176, 372)
+	item_button.size = Vector2(120, 40)
+	item_button.pressed.connect(_on_item_pressed)
+	add_child(item_button)
 
-func _resolve_battle() -> void:
-	var attacker = ActorStateScript.from_dictionary(DataRepository.get_actor("hero_yun"))
-	var defender = ActorStateScript.from_dictionary(DataRepository.get_actor(_enemy_id()))
-	var martial_art = MartialArtRecordScript.from_dictionary(DataRepository.get_martial_art("basic_sword"))
-	var result = CombatSystemScript.new().resolve_duel(attacker, defender, martial_art)
-	output.text = "\n".join(PackedStringArray(result.log))
-	var payload = result.to_dictionary()
-	payload["victory"] = result.winner_id == "hero_yun"
-	payload["source_object_id"] = str(context.get("source_object_id", ""))
-	payload["quest_id"] = str(context.get("quest_id", ""))
+	retreat_button = Button.new()
+	retreat_button.text = "暂退"
+	retreat_button.position = Vector2(320, 372)
+	retreat_button.size = Vector2(120, 40)
+	retreat_button.pressed.connect(_on_retreat_pressed)
+	add_child(retreat_button)
+
+func _on_attack_pressed() -> void:
+	combat_system.resolve_player_attack(battle_state, GameState, "basic_sword")
+	_refresh()
+	_return_if_finished()
+
+func _on_item_pressed() -> void:
+	combat_system.resolve_player_item(battle_state, GameState, "herb_small")
+	_refresh()
+	_return_if_finished()
+
+func _on_retreat_pressed() -> void:
+	combat_system.resolve_retreat(battle_state)
+	_refresh()
+	_return_if_finished()
+
+func _refresh() -> void:
+	var enemy_name = DataRepository.get_actor(_enemy_id()).get("name", "山道强人")
+	title_label.text = "战斗：%s" % enemy_name
+	hero_hp_label.text = "云游少侠 气血：%d / %d" % [battle_state.hero_hp, battle_state.hero_max_hp]
+	enemy_hp_label.text = "%s 气血：%d / %d" % [enemy_name, battle_state.enemy_hp, battle_state.enemy_max_hp]
+	output.text = "\n".join(PackedStringArray(battle_state.log))
+
+	var finished = battle_state.is_finished
+	attack_button.disabled = finished
+	item_button.disabled = finished
+	retreat_button.disabled = finished
+
+func _return_if_finished() -> void:
+	if not battle_state.is_finished:
+		return
+	var payload = battle_state.to_result_dictionary()
 	GameState.apply_battle_result(payload)
 	EventBus.battle_finished.emit(payload)
 	call_deferred("_return_to_map")
 
-func _retreat() -> void:
-	GameState.apply_battle_result({
-		"victory": false,
-		"source_object_id": str(context.get("source_object_id", "")),
-		"quest_id": str(context.get("quest_id", "")),
-	})
-	call_deferred("_return_to_map")
-
 func _return_to_map() -> void:
+	var source_map_id = battle_state.source_map_id
+	if source_map_id.is_empty():
+		source_map_id = str(context.get("source_map_id", GameState.map_state.current_map_id))
+	if source_map_id.is_empty():
+		source_map_id = "mountain_pass"
 	GameState.consume_battle_context()
-	SceneLoader.change_scene("res://scenes/mountain_pass.tscn")
+	SceneLoader.change_scene(GameState.get_scene_path_for_map(source_map_id))
 
 func _enemy_id() -> String:
 	var enemy_id = str(context.get("enemy_id", "bandit_01"))
